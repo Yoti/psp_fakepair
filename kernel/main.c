5f4f503056b137189ab59b2848edca8efa47670b
Yoti <realyoti@gmail.com>
2017-08-14 12:51:05 +0300

    v0.4

--- kernel/main.c
+++ kernel/main.c
@@ -1,13 +1,6 @@
 #include <pspsdk.h>
-#include <pspkernel.h>
-#include <pspdebug.h>
-#include <psploadcore.h>
-#include <pspiofilemgr.h>
-#include <pspdisplay.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <pspidstorage.h>
+//#include <pspidstorage.h>
 #include <pspsysmem_kernel.h>
 
 PSP_MODULE_INFO("kernel", 0x1006, 0, 4);
@@ -23,13 +16,13 @@ int pspKernelGetModel(void)
 	return generation;
 }
 
-void pspIdStorageLookup(u16 key, u32 offset, void *buf, u32 len)
-{
-	int k1 = pspSdkSetK1(0);
-	memset(buf, 0, len);
-	sceIdStorageLookup(key, offset, buf, len);
-	pspSdkSetK1(k1);
-}
+//void pspIdStorageLookup(u16 key, u32 offset, void *buf, u32 len)
+//{
+//	int k1 = pspSdkSetK1(0);
+//	memset(buf, 0, len);
+//	sceIdStorageLookup(key, offset, buf, len);
+//	pspSdkSetK1(k1);
+//}
 
 int module_start(SceSize args, void *argp)
 {
